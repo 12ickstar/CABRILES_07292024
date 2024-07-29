28ab3fd8c9e4823c5e623e75740158bec5f92dda
Ricky Cabriles <ricky.cabriles@gmail.com>
2024-07-29 23:21:01 +0800

    create home page, streaming page, and upload page modules

--- exam.client/src/app/app-routing.module.ts
+++ exam.client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'upload', loadChildren: () => import('./upload/upload.module').then(m => m.UploadModule) },
+  { path: 'streaming', loadChildren: () => import('./streaming/streaming.module').then(m => m.StreamingModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
