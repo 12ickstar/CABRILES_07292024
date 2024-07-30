eb078be7231691640ce0fbaa1125352ca6ae7afb
Ricky Cabriles <ricky.cabriles@gmail.com>
2024-07-30 09:20:14 +0800

    Create Unit Tests - (Mapping tests, validation exception tests)

---
+++ Exam.Application.UnitTest/Common/Mappings/MappingTests.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Exam.Application.Common.Mappings;
+using Exam.Application.Uploads.Queries.GetUploadById;
+using Exam.Domain.Entities;
+using System.Runtime.Serialization;
+
+namespace Exam.Application.UnitTest.Common.Mappings;
+
+public class MappingTests
+{
+    private readonly IConfigurationProvider _configuration;
+    private readonly IMapper _mapper;
+
+    public MappingTests()
+    {
+        _configuration = new MapperConfiguration(config =>
+            config.AddProfile<MappingProfile>());
+
+        _mapper = _configuration.CreateMapper();
+    }
+
+    [Test]
+    public void ShouldHaveValidConfiguration()
+    {
+        _configuration.AssertConfigurationIsValid();
+    }
+
+    [Test]
+    [TestCase(typeof(Upload), typeof(UploadDto))]
+    public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+    {
+        var instance = GetInstanceOf(source);
+
+        _mapper.Map(instance, source, destination);
+    }
+
+    private object GetInstanceOf(Type type)
+    {
+        if (type.GetConstructor(Type.EmptyTypes) != null)
+            return Activator.CreateInstance(type)!;
+
+        return FormatterServices.GetUninitializedObject(type);
+    }
+}
